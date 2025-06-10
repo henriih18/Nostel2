103bc7fcfa9041b29e8a8936dd3b2fd82113934c
riyo <hernandezhenry58@gmail.com>
2025-06-10 16:14:14 -0500

    Ajuste

--- src/main/java/Sena/ProyectoNostel/Application.java
+++ src/main/java/Sena/ProyectoNostel/Application.java
@@ -1,16 +1,13 @@
 package Sena.ProyectoNostel;
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-import javax.crypto.SecretKey;
-import java.util.Base64;
+
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"Sena.ProyectoNostel.domain.service", "Sena.ProyectoNostel.persistence", "Sena.ProyectoNostel.web.controller"})
+
 public class Application {
 
 	public static void main(String[] args) {
