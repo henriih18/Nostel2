68ca46f77c7b623dc8db3d968c974c2cec5289dc
riyo <hernandezhenry58@gmail.com>
2025-06-10 22:41:01 -0500

    Ajuste

--- src/main/java/Sena/ProyectoNostel/web/controller/RootController.java
+++ src/main/java/Sena/ProyectoNostel/web/controller/RootController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @GetMapping("/")
+    @GetMapping({"/", ""})
     public ResponseEntity<Void> root() {
         return ResponseEntity.ok().build();
     }
