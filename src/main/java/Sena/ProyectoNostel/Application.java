c2333f1085490082f32445220efe18abd17a41c7
riyo <hernandezhenry58@gmail.com>
2025-06-10 20:36:45 -0500

    Ajuste

--- src/main/java/Sena/ProyectoNostel/Application.java
+++ src/main/java/Sena/ProyectoNostel/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println(">> SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"));
 		SpringApplication.run(Application.class, args);
 
 		/*SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
